3a1f4e10dfd7518561964b889caac9bcb0d114c3
OKharchenko <okharchenko@luxoft.com>
2023-01-10 16:14:11 +0200

    spring boot initialized

---
+++ src/main/java/com/example/gitsquashtraining/GitSquashTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitsquashtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSquashTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitSquashTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitsquashtraining/GitSquashTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitsquashtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSquashTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
